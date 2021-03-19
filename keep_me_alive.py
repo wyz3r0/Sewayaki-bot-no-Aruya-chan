0e3a143f7267d89cca1be4db49afb771534b77a5
Wyzero <dobroszek.d@gmail.com>
2021-03-19 12:18:29 +0000

    v0.0.2 manual loading and unloading cogs

--- keep_me_alive.py
+++ keep_me_alive.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hai~ Hai~"
+  return "Hai~ Hai~"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
 def keep_me_alive():
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from discord.ext import commands
 from keep_me_alive import keep_me_alive
 
-client = commands.Bot(command_prefix='>')
+client = commands.Bot(command_prefix = '>')
 
 @client.event
 async def on_ready():
@@ -12,15 +12,18 @@ async def on_ready():
 
 @client.command()
 async def load(ctx, extension):
-  client.load_extencion(f'cogs.{extension}')
+  client.load_extension(f'cogs.{extension}')
+  print(f'{extension} successfully loaded')
 
 @client.command()
 async def unload(ctx, extension):
-  client.unload_extencion(f'cogs.{extension}')
+  client.unload_extension(f'cogs.{extension}')
+  print(f'{extension} successfully unloaded')
 
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
+    client.load_extension(f'cogs.{filename[:-3]}') 
+    print(f'{filename} successfully loaded')
 
 keep_me_alive()
 client.run(os.getenv('token'))
