a250f57464b8424dd9cf56b9f3fdf65f35187437
Wyzero <dobroszek.d@gmail.com>
2021-03-19 00:51:57 +0000

    commands not events and cogs

---
+++ cogs/answers.py
@@ -0,0 +1,18 @@
+
+import discord
+from discord.ext import commands
+
+class answers(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+  @commands.command()
+  async def ping(self, ctx):
+    await ctx.send(f'> pong {round(self.client.latency * 1000)}ms')
+
+  @commands.command()
+  async def hello(self, ctx):
+    await ctx.send("> Hellow~")
+
+def setup(client):
+  client.add_cog(answers(client))
--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 
 import discord
 import os
+from discord.ext import commands
 from keep_me_alive import keep_me_alive
-from replit import db
 
-client = discord.Client()
-
-symbol = '>'
-
-def 
+client = commands.Bot(command_prefix='>')
 
 @client.event
 async def on_ready():
   print('Ohayo gozaimasu~ Aruyadesu')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith(symbol + 'hello'):
-    await message.channel.send('Hellow~')
+@client.command()
+async def load(ctx, extension):
+  client.load_extencion(f'cogs.{extension}')
 
-  if message.content.startswith(symbol + 'symbol'):
-    await message.channel.send('So what do you want me to react to?')
+@client.command()
+async def unload(ctx, extension):
+  client.unload_extencion(f'cogs.{extension}')
 
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    client.load_extension(f'cogs.{filename[:-3]}')
 
 keep_me_alive()
 client.run(os.getenv('token'))
